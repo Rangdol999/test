b1edadba7e7511de88b9e1ca9fb2e84f23299d2b
Rangdol999 <89280824+Rangdol999@users.noreply.github.com>
2022-06-01 16:45:48 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,14 +1,3 @@
 print('Namasta France')
 print('Namasta France')
 print('Namasta France')
-
-print('Namasta France')
-print('Namasta France')
-print('Namasta France')
-
-
-print('Tashi Deleck')
-
-
-print("gabriel")
-
